343d9b437cdf125169e663cbd3b0e992b58e3e6d
trow <trow@localhost>
2009-05-18 17:49:37 +0200

    Removes an unnecessary import.

--- django-extras/contrib/auth/__init__.py
+++ django-extras/contrib/auth/__init__.py
@@ -22,8 +22,6 @@ django.contrib.auth.  Here we put in a few stub APIs, just enough to
 keep the imports from failing.
 """
 
-from auth.models import User
-
 
 def authenticate(**credentials):
     raise NotImplementedError
