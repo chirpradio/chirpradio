f939148fc159324e04907d3f58cdd70a11c323b6
kumar <kumar.mcmillan@gmail.com>
2009-07-23 09:37:04 -0500

    Reduced delay on autocomplete

--- media/playlists/js/playlists.js
+++ media/playlists/js/playlists.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     var default_opt = {
         selectFirst: false,
         minChars: 3,
-        delay: 600,
+        delay: 400,
         maxItemsToShow: 15,
         matchContains: true // tells the cache to do substring matches 
                             // (necessary when searching "eno" and the 
