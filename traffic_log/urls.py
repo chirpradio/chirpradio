2770392c6406f9058892ef6921c5db6587e6347b
james yoo <james.yoo@gmail.com>
2009-08-21 00:24:38 -0500

    added generate url

--- traffic_log/urls.py
+++ traffic_log/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
     (r'^spot/(?P<spot_key>[^\.^/]+)/?$', 'traffic_log.views.spotDetail'),                       
     (r'^spot_constraint/delete/(?P<spot_constraint_key>[^\.^/]+)/spot/(?P<spot_key>[^\.^/]+)?$',
      'traffic_log.views.deleteSpotConstraint'),
+    (r'^generate/(\d{4})/(\d{2})/(\d{2})','traffic_log.views.generateTrafficLog')
     )
 
