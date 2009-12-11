f9435d1d28937ace0998ae21ce8b5d9ce375f17b
ddurham <ddurham@localhost.localdomain>
2009-12-10 20:48:47 -0600

    fixed syspath issues

--- auth/user_loader.py
+++ auth/user_loader.py
@@ -2,6 +2,12 @@ import datetime
 from google.appengine.ext import db
 from google.appengine.tools import bulkloader
 from google.appengine.api import datastore_types
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from auth import models
 
 #    email:4,first_name:2,last_name:3,password,is_active:7,is_superuser:8
 #    last_login:9,date_joined:10,roles
@@ -15,8 +21,8 @@ class UserLoader(bulkloader.Loader):
                                     ('password', str),
                                     ('is_active', bool),
                                     ('is_superuser', bool),
-                                    ('last_login', lambda x: datetime.datetime.fromtimestamp(x)),
-                                    ('date_joined',lambda x: datetime.datetime.fromtimestamp(x)),
+                                    ('last_login', lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')),
+                                    ('date_joined',lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')),
                                     ('roles', str.split)
                                    ])   
 
