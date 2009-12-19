89d68ce62ac15362aed5b907922caa62a335121a
kumar <kumar.mcmillan@gmail.com>
2009-12-18 18:49:48 -0600

    Fixed Live Read PSA spot type and added Station ID spot type (all per Shawn's feedback)

--- traffic_log/constants.py
+++ traffic_log/constants.py
@@ -8,10 +8,11 @@ SLOT = (0,12,13,24,25,30,36,37,48,49)
 SPOT_TYPE = [
     "Live Read Promo",
     "Recorded Promo",
-    "Live Read Promo",
+    "Live Read PSA",
     "Recorded PSA",
     "Underwriting Spot",
     "Pledge Liner",
+    "Station ID",
     "Other"
     ]
 
