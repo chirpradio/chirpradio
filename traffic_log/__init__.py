a6c3e75fe5c4a5b3dc060f2b595fc639db43ec97
kumar <kumar.mcmillan@gmail.com>
2009-12-18 19:21:18 -0600

    Added docstring explaining the Traffic Log app

---
+++ traffic_log/__init__.py
@@ -0,0 +1,5 @@
+
+"""Manage and track actions that on-air DJs need to perform (usually during breaks in between songs).
+
+For example, a DJ might need to read an announcement about an upcoming CHIRP event.  The text for the event needs to be read verbatim and cannot be improvised.  After reading it, the DJ needs to mark it as "done" for tracking purposes.  There are several different kinds of traffic log "spots" -- reading the stations ID, playing a pre-recorded PSA, reading a promo from one of our underwriters, etc.
+"""
