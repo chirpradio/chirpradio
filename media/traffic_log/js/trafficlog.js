a3dcdeea83e2b2ae5b797f424a41ff9a4d8524ad
kumar <kumar.mcmillan@gmail.com>
2009-12-29 19:34:38 -0600

    cleaned up javascript code a bit and implemented the refresh button on the traffic log spot viewer

--- media/traffic_log/js/trafficlog.js
+++ media/traffic_log/js/trafficlog.js
@@ -1,14 +1,32 @@
+// requires: chirp/chirp.js
+//           
+
+$(document).ready(function() {
+    $("#refresh-button").click(function() {
+        window.location = "/traffic_log/?t=" + Math.random().toString();
+    });
+});
 
 
-google.load("jquery", "1");
-google.load("jqueryui", "1");
+// Kumar: might use this timer to display a notice but not a redirect
+// because a DJ might be reading a spot when the timeout occurs
 
-function OnLoad(){
-  $('#content').html('<div id="draggable-handle-div" style="width:100px;border:1px solid #999;">' +
-                     '<div style="background-color:#999">dragme</div>content</div>');
-  $("#draggable-handle-div").draggable({
-    "handle": "div"
-  });
-}
+/*var start_timer = function() {
+    var time = new Date();
+    var hour = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    if ( seconds == 0 && minutes == 0 ) {
+        window.location.replace( unescape( window.location.pathname ) ); 
+    } else {
+		if( minutes < 10 ){
+			minutes = '0' + minutes;
+		} 
+		if ( seconds < 10 ){
+			seconds = '0' + seconds;
+		}
 
-google.setOnLoadCallback(OnLoad);
+        self.status = hour + ":" + minutes +":" + seconds;
+        self.setTimeout("start_timer()", 60)
+    }
+}*/
