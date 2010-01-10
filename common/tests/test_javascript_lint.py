8fe4758d6819ee17b19e4b0ce9a7dc2b32ab5196
kumar <kumar.mcmillan@gmail.com>
2010-01-10 15:46:09 -0600

    Better exception message when jsl command doesn't exist

--- common/tests/test_javascript_lint.py
+++ common/tests/test_javascript_lint.py
@@ -1,10 +1,14 @@
 
+import sys
 import subprocess
 import glob
 import os
 import unittest
 
 __all__ = ['TestJavascriptLint']
+
+class NoSuchCommand(OSError):
+    pass
 
 class TestJavascriptLint(unittest.TestCase):
     
@@ -15,16 +19,25 @@ class TestJavascriptLint(unittest.TestCase):
         assert os.path.exists(media), "Possible miscalculation of root: %s" % root
         
         for filename in glob.glob("%s/*/js/*.js" % media):
-            # jsl -nologo -nosummary -process media/playlists/js/playlists.js
-            p = subprocess.Popen([
-                'jsl',
-                '-nologo',
-                '-nosummary',
-                '-process',
-                filename
-                ],
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.STDOUT)
+            # in macports:
+            # sudo port install javascript-lint
+            prog = 'jsl'
+            try:
+                p = subprocess.Popen([
+                    prog,
+                    '-nologo',
+                    '-nosummary',
+                    '-process',
+                    filename
+                    ],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+            except OSError, exc:
+                etype, val, tb = sys.exc_info()
+                raise NoSuchCommand(
+                    "%s: %s (please make sure javascript-lint `%s` exists and is in your $PATH)" % (
+                                                                    etype.__name__, val, prog)), None, tb
+                
             returncode = p.wait()
             if returncode != 0:
                 print "*"*40
