0c4b2e6b627e02c41b6b22f15eb7acb89821dca5
mike <mike@chirpradio.org>
2010-02-01 16:57:36 -0600

    Front page of Traffic Log styled

--- media/traffic_log/js/trafficlog.js
+++ media/traffic_log/js/trafficlog.js
@@ -29,31 +29,40 @@ $(document).ready(function() {
     //     $('#facebox table').width(700);
     // });
     
-    $(".show-text-for-reading").click(function(e) {
-        e.preventDefault();
-        var url = $(this).attr("href");
-        var tr = $(this).parent().parent(); // a->td->tr
-        $.facebox(function() {
-            $.ajax({
-                url: url,
-                success: function(data, textStatus) {
-                    $.facebox(data);
-                    var onclick = function(e) {
-                        e.preventDefault();
-                        var anchor = this;
-                        ns.handle_finish_spot(anchor, tr);
-                        $(document).trigger('close.facebox');
-                        $(this).unbind("click", onclick);
-                    };
-                    $('.finish-spot-after-reading').click(onclick);
-                },
-                error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    alert("Whoops, there was an error on the server. An email has been sent to the admins so sit tight or try again.");
-                    $(document).trigger('close.facebox');
-                }
-            });
-        });
+    $('#modal-container').jqm({
+      ajax: '@href',
+      overlay: 75,
+      trigger: '.show-text-for-reading'
     });
+    
+//    $(".show-text-for-reading").click(function(e) {
+//        e.preventDefault();
+//        var $this = $(this);
+//        var url = $(this).attr("href");
+//        var tr = $(this).parent().parent(); // a->td->tr
+
+//        $.facebox(function() {
+//            $.ajax({
+//                url: url,
+//                success: function(data, textStatus) {
+//                    $.facebox(data);
+//                    var onclick = function(e) {
+//                        e.preventDefault();
+//                        var anchor = this;
+//                        ns.handle_finish_spot(anchor, tr);
+//                        $(document).trigger('close.facebox');
+//                        $(this).unbind("click", onclick);
+//                    };
+//                    $('.finish-spot-after-reading').click(onclick);
+//                },
+//                error: function (XMLHttpRequest, textStatus, errorThrown) {
+//                    alert("Whoops, there was an error on the server. An email has been sent to the admins so sit tight or try again.");
+//                    $(document).trigger('close.facebox');
+//                }
+//            });
+//        });
+        
+//    });
     
     $(".finish-spot").click(function(e) {
         e.preventDefault();
@@ -63,39 +72,39 @@ $(document).ready(function() {
     });
     
     // sigh. this is all necessary to override the image paths:
-    $.extend($.facebox.settings, {
-        loadingImage : '/media/common/js/jquery-facebox/loading.gif',
-        closeImage   : '/media/common/js/jquery-facebox/closelabel.gif',
-        faceboxHtml  : '\
-    <div id="facebox" style="display:none;"> \
-      <div class="popup"> \
-        <table> \
-          <tbody> \
-            <tr> \
-              <td class="tl"/><td class="b tb"/><td class="tr"/> \
-            </tr> \
-            <tr> \
-              <td class="b"/> \
-              <td class="body"> \
-                <div class="content"> \
-                </div> \
-                <div class="footer"> \
-                  <a href="#" class="close"> \
-                    <img src="/media/common/js/jquery-facebox/closelabel.gif" title="close" class="close_image" /> \
-                  </a> \
-                </div> \
-              </td> \
-              <td class="b"/> \
-            </tr> \
-            <tr> \
-              <td class="bl"/><td class="b"/><td class="br"/> \
-            </tr> \
-          </tbody> \
-        </table> \
-      </div> \
-    </div>'
-        }
-    );
+//    $.extend($.facebox.settings, {
+//        loadingImage : '/media/common/js/jquery-facebox/loading.gif',
+//        closeImage   : '/media/common/js/jquery-facebox/closelabel.gif',
+//        faceboxHtml  : '\
+//    <div id="facebox" style="display:none;"> \
+//      <div class="popup"> \
+//        <table> \
+//          <tbody> \
+//            <tr> \
+//              <td class="tl"/><td class="b tb"/><td class="tr"/> \
+//            </tr> \
+//            <tr> \
+//              <td class="b"/> \
+//              <td class="body"> \
+//                <div class="content"> \
+//                </div> \
+//                <div class="footer"> \
+//                  <a href="#" class="close"> \
+//                    <img src="/media/common/js/jquery-facebox/closelabel.gif" title="close" class="close_image" /> \
+//                  </a> \
+//                </div> \
+//              </td> \
+//              <td class="b"/> \
+//            </tr> \
+//            <tr> \
+//              <td class="bl"/><td class="b"/><td class="br"/> \
+//            </tr> \
+//          </tbody> \
+//        </table> \
+//      </div> \
+//    </div>'
+//        }
+//    );
 });
 
 
