3d4dc1d02766ebad4e860bc28ac5c9a61236f1a3
jawaxman <devnull@localhost>
2011-02-20 17:50:36 -0600

    Added no_album_cover.png.

--- djdb/views.py
+++ djdb/views.py
@@ -791,7 +791,7 @@ def album_info_page(request, album_id_str, ctx_vars=None):
         ctx_vars["album_cover_m"] = lastfm_album.get_cover_image(pylast.COVER_MEDIUM)
         ctx_vars["album_cover_xl"] = lastfm_album.get_cover_image(pylast.COVER_EXTRA_LARGE)
     except:
-        ctx_vars["album_cover_m"] = "/media/common/img/album.png"
+        ctx_vars["album_cover_m"] = "/media/common/img/no_cover_art.png"
         pass
 
     ctx_vars["album"] = album
