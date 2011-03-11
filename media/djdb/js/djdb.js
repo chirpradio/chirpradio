7b801681330989eabe737c90f8efdb9d7de0065d
jawaxman <devnull@localhost>
2011-03-10 22:00:17 -0600

    Added send to playlist tracker buttons to tracks on album info page in djdb.

--- media/djdb/js/djdb.js
+++ media/djdb/js/djdb.js
@@ -91,4 +91,31 @@ $(document).ready(function() {
         $(this).parents("form").submit();
         return false;
     });
+
+    $("a.send_to_playlist").unbind('click').click(function() {
+        var img = jQuery("img", this);
+        $.ajax({
+            url: $(this).attr("href"),
+            type: 'GET',
+            error: function(data) {
+                $('div.error').append(data.responseText);
+
+                var win = window.open('','Error Sending to Playlist','scrollbars=1,width=600,height=400');
+                var html = data.responseText;
+                win.document.open();
+                win.document.write(html);
+                win.document.close();
+
+                img.attr("src", "/media/common/img/play-error.png");
+                setTimeout(function() { img.attr("src", "/media/common/img/play.png"); }, 1000);
+            },
+            success: function(data) {
+                document.cookie = "chirp_track_to_play=" + data + "; path=/";
+                img.attr("src", "/media/common/img/play-success.png");
+                setTimeout(function() { img.attr("src", "/media/common/img/play.png"); }, 1000);
+            }
+        });
+
+        return false;
+    });
 });
--- media/djdb/js/djdb_crate.js
+++ media/djdb/js/djdb_crate.js
@@ -13,32 +13,6 @@ $(document).ready(function() {
         $("div.remove_all_crate_items").slideUp("slow");        
         return false;
     });
-    $("a.send_to_playlist").unbind('click').click(function() {
-        var img = jQuery("img", this);
-        $.ajax({
-            url: $(this).attr("href"),
-            type: 'GET',
-            error: function(data) {
-                $('div.error').append(data.responseText);
-
-                var win = window.open('','Error Sending to Playlist','scrollbars=1,width=600,height=400');
-                var html = data.responseText;
-                win.document.open();
-                win.document.write(html);
-                win.document.close();
-
-                img.attr("src", "/media/common/img/play-error.png");
-                setTimeout(function() { img.attr("src", "/media/common/img/play.png"); }, 1000);
-            },
-            success: function(data) {
-                document.cookie = "chirp_track_to_play=" + data + "; path=/";
-                img.attr("src", "/media/common/img/play-success.png");
-                setTimeout(function() { img.attr("src", "/media/common/img/play.png"); }, 1000);
-            }
-        });
-
-        return false;
-    });
 
     $('#id_is_heavy_rotation').click(function(e) {
         if ($(this).is(':checked')) {
