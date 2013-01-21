a7506818309bbcfc57aa1134a06e05a0010c9414
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-01-21 14:44:24 -0600

    Removes old helper to get dev_appserver working
    Note that the test suite does not run with
    this commit

--- settings.py
+++ settings.py
@@ -33,12 +33,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'appengine'  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -117,7 +112,6 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-     'appengine_django',
      'auth',
      'common',
      'djdb',
