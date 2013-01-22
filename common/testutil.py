f829ac1efe07a4228af72ef5f0331ac7f255fa73
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-01-21 23:47:48 -0600

    Removed djangoforms from common testutils

--- common/testutil.py
+++ common/testutil.py
@@ -1,21 +1,21 @@
 
 """Common test suite utilities."""
 
-from django import forms as djangoforms
+import django.forms
 
 class FormTestCaseHelper(object):
     """Mixin for unittest.TestCase descendants to aid in testing forms."""
-    
+
     def assertNoFormErrors(self, response):
         if response.context is None:
             return
-        # if all went well the request probably redirected, 
+        # if all went well the request probably redirected,
         # otherwise, there will be form objects with errors:
         forms = []
         for ctx in response.context:
             for cdict in ctx:
                 for v in cdict.values():
-                    if isinstance(v, djangoforms.BaseForm):
+                    if isinstance(v, django.forms.BaseForm):
                         forms.append(v)
         for form in forms:
             self.assertEquals(form.errors.as_text(), "")
