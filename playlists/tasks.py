d07fea7cb1468d9a5b926c25ca944a34dd319e57
Noel Taylor <ntaylor@imagescape.com>
2018-05-14 16:35:30 -0500

    remove now-deprecated config options from app.yaml; call Google cloud function to tweet 'now-playing' track when playlist is updated

--- playlists/tasks.py
+++ playlists/tasks.py
@@ -147,7 +147,8 @@ def send_track_to_live_site(request):
     """View for task queue that tells chirpradio.org a new track was entered"""
     log.info('Pushing notifications for track %r' % request.POST['id'])
     success = [_push_notify('chirpradio.push.recently-played'),
-               _push_notify('chirpradio.push.now-playing')]
+               _push_notify('chirpradio.push.now-playing'),
+               _push_notify('chirpradio.push.tweet-now-playing')]  # this calls a Google Cloud Function
     if all(success):
         return HttpResponse("OK")
     else:
